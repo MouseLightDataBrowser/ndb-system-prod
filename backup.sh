597b4c80b755c0209a3cd8dbc19a39f1a47ef8c3
Patrick Edson <pedson@users.noreply.github.com>
2017-10-11 15:55:21 -0400

    Pass backup volume location.

--- backup.sh
+++ backup.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -it --rm --network ndb_back_tier -e NODE_ENV=production -v /opt/data:/opt/data mouselightdatabrowser/data
+if [ -z ${BACKUP_VOL} ]; then
+    export BACKUP_VOL="/opt/data/backups"
+fi
+
+docker run -it --rm --network ndb_back_tier -e BACKUP_VOL=${BACKUP_VOL} -e NODE_ENV=production -v /opt/data:/opt/data mouselightdatabrowser/data
