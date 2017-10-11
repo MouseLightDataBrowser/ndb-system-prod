716af0ca8d7577db72f58814a46e61b8e82d60e1
Patrick Edson <pedson@users.noreply.github.com>
2017-10-11 15:58:46 -0400

    Mount additional volume.

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ if [ -z ${BACKUP_VOL} ]; then
     export BACKUP_VOL="/opt/data/backups"
 fi
 
-docker run -it --rm --network ndb_back_tier -e BACKUP_VOL=${BACKUP_VOL} -e NODE_ENV=production -v /opt/data:/opt/data mouselightdatabrowser/data
+docker run -it --rm --network ndb_back_tier -e BACKUP_VOL=${BACKUP_VOL} -e NODE_ENV=production -v /opt/data:/opt/data -v /mnt/groups:/mnt/groups mouselightdatabrowser/data
