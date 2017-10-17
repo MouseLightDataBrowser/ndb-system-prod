8c676eea56e7612533a9ec23ddd8a94b4b69aaa6
Patrick Edson <pedson@users.noreply.github.com>
2017-10-17 13:15:22 -0400

    Add load balancer, two front-end instances.

--- down.sh
+++ down.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "${TRANSFORM_VOL}" ]; then
+    export TRANSFORM_VOL="/mnt/groups"
+fi
+
+if [ -z "${DATA_VOL}" ]; then
+    export DATA_VOL="/opt/data"
+fi
+
 docker-compose -p ndb down
--- up.sh
+++ up.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -z "${TRANSFORM_VOL}" ]; then
-#   Until ml-ubuntu-test has standard mounts
-#   export TRANSFORM_VOL="/groups/mousebrainmicro/mousebrainmicro"
     export TRANSFORM_VOL="/mnt/groups"
+fi
+
+if [ -z "${DATA_VOL}" ]; then
+    export DATA_VOL="/opt/data"
 fi
 
 docker-compose -p ndb up -d
