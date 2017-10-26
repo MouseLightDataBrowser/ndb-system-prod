fe06a4511bb32743ff97484d3c0cc4c5355ca92d
Patrick Edson <pedson@users.noreply.github.com>
2017-10-25 21:00:02 -0400

    Update for latest configuration.

--- up.sh
+++ up.sh
@@ -4,8 +4,12 @@ if [ -z "${TRANSFORM_VOL}" ]; then
     export TRANSFORM_VOL="/mnt/groups"
 fi
 
-if [ -z "${DATA_VOL}" ]; then
-    export DATA_VOL="/opt/data"
+if [ -z "${BACKUP_DATA_VOL}" ]; then
+    export BACKUP_DATA_VOL="/opt/data"
+fi
+
+if [ -z "${EXPORT_DATA_VOL}" ]; then
+    export EXPORT_DATA_VOL="/opt/data"
 fi
 
 docker-compose -p ndb up -d
