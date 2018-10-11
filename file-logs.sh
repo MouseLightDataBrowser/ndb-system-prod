31382900cf2eb008093ae31c84006e0f9d3b99e5
Patrick Edson <pedson@users.noreply.github.com>
2018-10-10 20:16:43 -0400

    Fix mount name.

--- file-logs.sh
+++ file-logs.sh
@@ -3,4 +3,4 @@
 source ./.env
 source ./options.sh
 
-docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -v ${MNB_COMPOSE_PROJECT}_log_output:/var/log/pipeline busybox
+docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -v ${MNB_COMPOSE_PROJECT}_log_output:/var/log/mnb busybox
