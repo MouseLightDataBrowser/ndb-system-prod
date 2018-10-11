e6cd6e5f4b901595f97e2d60b0fc5112d5d3dbc1
Patrick Edson <pedson@users.noreply.github.com>
2018-10-10 20:08:47 -0400

    Add logging improvements.

---
+++ file-logs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source ./.env
+source ./options.sh
+
+docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -v ${MNB_COMPOSE_PROJECT}_log_output:/var/log/pipeline busybox
