d274fd0b2124267178b9f9074c3aa301a15372a3
Patrick Edson <pedson@users.noreply.github.com>
2018-10-23 15:27:27 -0400

    Fix script references.

--- backup.sh
+++ backup.sh
@@ -9,4 +9,4 @@ echo "${MNB_BACKUP_LOCATION}"
 test -d "${MNB_BACKUP_LOCATION}"  || echo "Backup location is not accessible - skipping"
 test -d "${MNB_BACKUP_LOCATION}"  || exit 0
 
-docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_BACKUP_LOCATION}":/opt/data/backup mouselightdatabrowser/data-services:1.4 backup.sh
+docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_BACKUP_LOCATION}":/opt/data/backup mouselightdatabrowser/data-services:1.4 ./backup.sh
--- restore.sh
+++ restore.sh
@@ -9,4 +9,4 @@ echo "Using restore location ${MNB_RESTORE_LOCATION}"
 test -d "${MNB_RESTORE_LOCATION}"  || echo "Restore location is not accessible - skipping"
 test -d "${MNB_RESTORE_LOCATION}"  || exit 0
 
-docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_RESTORE_LOCATION}":/opt/data/restore mouselightdatabrowser/data-services:1.4 restore.sh
+docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_RESTORE_LOCATION}":/opt/data/restore mouselightdatabrowser/data-services:1.4 ./restore.sh
