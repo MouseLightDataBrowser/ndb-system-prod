e4fff9743a387e73eb29ac59c933403206bb012c
Patrick Edson <pedson@users.noreply.github.com>
2019-01-15 21:18:57 -0500

    Separate local and remove backups.

--- backup.sh
+++ backup.sh
@@ -4,9 +4,15 @@ if [ -a "options.sh" ]; then
     source "options.sh"
 fi
 
-echo "${MNB_BACKUP_LOCATION}"
+echo "${MNB_LOCAL_BACKUP_LOCATION}"
+echo "${MNB_REMOTE_BACKUP_LOCATION}"
 
-test -d "${MNB_BACKUP_LOCATION}"  || echo "Backup location is not accessible - skipping"
-test -d "${MNB_BACKUP_LOCATION}"  || exit 0
+test -d "${MNB_LOCAL_BACKUP_LOCATION}"  || echo "local backup location is not accessible - skipping"
+test -d "${MNB_LOCAL_BACKUP_LOCATION}"  || exit 0
 
-docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_BACKUP_LOCATION}":/opt/data/backup mouselightdatabrowser/data-services:1.4 ./backup.sh
+test -d "${MNB_REMOTE_BACKUP_LOCATION}"  || echo "remote backup location is not accessible - skipping"
+test -d "${MNB_REMOTE_BACKUP_LOCATION}"  || exit 0
+
+docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_LOCAL_BACKUP_LOCATION}":/opt/data/ mouselightdatabrowser/data-services:1.4 ./backup.sh
+
+docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -e BACKUP_PATH=/opt/data/neuron-database -v "${MNB_REMOTE_BACKUP_LOCATION}":/opt/data/ mouselightdatabrowser/data-services:1.4 ./backup.sh
