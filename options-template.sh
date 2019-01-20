1d1055f63d88b95fd8fbb0dd5324fcbbed0d17da
Patrick Edson <pedson@users.noreply.github.com>
2019-01-20 12:49:02 -0500

    Missing entry in template.

--- options-template.sh
+++ options-template.sh
@@ -9,6 +9,8 @@ export MNB_COMPOSE_PROJECT=
 # Environment variables that used both in the Docker Compose files and various scripts.  Env var that just used in the
 # Docker Compose files are in the Compose specific file env.
 
+export MNB_LOG_VOLUME=/data/log/mnb
+
 # Location is mapped to the backup location in the data services container.  Backup files will appear in sample, swc,
 # transform, and search subdirectories within this location.  Note that this is _not_ mapped to the BACKUP_PATH env var
 # in the data services container.  It is used to map a local location to the default backup path in the container via a
