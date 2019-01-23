3c4a66cab757b57dda86f05bb114f2be61e86d35
Patrick Edson <pedson@users.noreply.github.com>
2019-01-23 08:11:19 -0500

    Improve backup/restore process and documentation.

--- options-template.sh
+++ options-template.sh
@@ -7,15 +7,53 @@ export DATABASE_PW=
 export MNB_COMPOSE_PROJECT=
 
 # Environment variables that used both in the Docker Compose files and various scripts.  Env var that just used in the
-# Docker Compose files are in the Compose specific file env.
+# Docker Compose files are in the Compose specific .env file.
 
-export MNB_LOG_VOLUME=/data/log/mnb
+# Will be mapped to /var/log/mnb in most service containers for log files.  Could be a host location or a docker volume.
+export MNB_LOG_VOLUME=
 
 # Location is mapped to the backup location in the data services container.  Backup files will appear in sample, swc,
 # transform, and search subdirectories within this location.  Note that this is _not_ mapped to the BACKUP_PATH env var
 # in the data services container.  It is used to map a local location to the default backup path in the container via a
 # volume mapping.
 export MNB_LOCAL_BACKUP_LOCATION=
+
+# For remote backups it is not assumed that the host location can be directly mapped to the container backup path.  For
+# instance, mounts that can not be accessed by docker containers launched as root.  The host path and the backup path in
+# in the container are both set in this case.
+
+# Typically set to the deepest path that can be mapped by docker.
 export MNB_REMOTE_BACKUP_LOCATION=
 
+# Overrides the container path where backups are saved.  If MNB_REMOTE_BACKUP_LOCATION can be set to the full backup
+# location, this can be left empty and the container default will be used.
+export MNB_REMOTE_BACKUP_PATH=
+
+# The following are analogous to remote backup location/path.  If the additional redirection is not required for
+# permissions or other reasons, MNB_RESTORE_PATH can be left empty and the default will be used.
 export MNB_RESTORE_LOCATION=
+
+export MNB_RESTORE_PATH=
+
+# Additional Details - How Backups Work
+#
+# Backups in the container are saved to subdirectories within the directory specified by BACKUP_PATH within the
+# container.  The default for this location if /opt/data/backup if that is not overridden in container environment
+# variables.
+#
+# For example, mapping the host path /data/mnb to /opt/data in the container would result in backups to
+# /data/mnb/backup/swc/, /data/mnb/backup/sample/, etc. on the host.
+#
+# For "local" backups, it is assumed to be on the host machine and that docker, as root, is able to mount any path on
+# the host as read/write.  Therefore only MNB_LOCAL_BACKUP_LOCATION is required and this location is mapped to
+# /opt/data in the container and backups will be to subdirectories in ${MNB_LOCAL_BACKUP_LOCATION}/backup/.
+#
+# For "remote" backups, it is not assumed that the full path of the backup location is usable by root.  For example,
+# root may have access to the mount point of a network share at /mnt/foo but not have read/write access to the desired
+# backup location on the share of /mnt/foo/projects/bar/backup/.  Therefore, for remote backups both a host path and the
+# container backup path may be specified.
+#
+# In the example above, MNB_REMOTE_BACKUP_PATH would be set to /mnt/foo to allow the volume mapping to the container (it
+# will be mapped to /opt/data) and and MNB_REMOTE_BACKUP_PATH (used to override BACKUP_PATH in the container) would be
+# set to /opt/data/projects/bar/backup/ (it is assumed that unlike root on the host, the user in the data-services
+# container has read/write access to the final location on the mounted share).
--- restore.sh
+++ restore.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ -a "options.sh" ]; then
+if [[ && "options.sh" ]]; then
     source "options.sh"
 fi
 
-echo "Using restore location ${MNB_RESTORE_LOCATION}"
-
-test -d "${MNB_RESTORE_LOCATION}"  || echo "Restore location is not accessible - skipping"
-test -d "${MNB_RESTORE_LOCATION}"  || exit 0
-
-docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_RESTORE_LOCATION}":/opt/data/restore mouselightdatabrowser/data-services:1.4 ./restore.sh
+if [[ test -d "${MNB_RESTORE_LOCATION}" ]]; then
+    echo "attempting restore from ${MNB_RESTORE_LOCATION}"
+    docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -e RESTORE_PATH=${MNB_RESTORE_PATH} -v "${MNB_RESTORE_LOCATION}":/opt/data mouselightdatabrowser/data-services:1.4 ./restore.sh
+else
+    echo "local restore location ${MNB_RESTORE_LOCATION} is not accessible - skipping"
+fi
