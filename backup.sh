042c1daa61225900cc591091e0fa660ba4e59586
Patrick Edson <pedson@users.noreply.github.com>
2019-01-23 11:00:02 -0500

    Same as last.

--- backup.sh
+++ backup.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-if [ -a "options.sh" ]; then
+if [[ -a "options.sh" ]]; then
     source "options.sh"
 fi
 
-if [ test -d "${MNB_LOCAL_BACKUP_LOCATION}" ]; then
+haveLocal=test -d "${MNB_LOCAL_BACKUP_LOCATION}"
+
+if [[ haveLocal ]]; then
     echo "attempting backup to ${MNB_LOCAL_BACKUP_LOCATION}"
     docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_LOCAL_BACKUP_LOCATION}":/opt/data/ mouselightdatabrowser/data-services:1.4 ./backup.sh
 else
     echo "local backup location ${MNB_LOCAL_BACKUP_LOCATION} is not accessible - skipping"
 fi
 
-if [ test -d "${MNB_REMOTE_BACKUP_LOCATION}" ]; then
+haveRemote=test -d "${MNB_LOCAL_BACKUP_LOCATION}"
+
+if [[ haveRemote ]]; then
     echo "attempting backup to ${MNB_REMOTE_BACKUP_LOCATION}"
     docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -e BACKUP_PATH=${MNB_REMOTE_BACKUP_PATH} -v "${MNB_REMOTE_BACKUP_LOCATION}":/opt/data mouselightdatabrowser/data-services:1.4 ./backup.sh
 else
--- restore.sh
+++ restore.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ && "options.sh" ]]; then
+if [[ -a "options.sh" ]]; then
     source "options.sh"
 fi
 
-if [[ test -d "${MNB_RESTORE_LOCATION}" ]]; then
+haveRemote=test -d "${MNB_RESTORE_LOCATION}"
+
+if [[ haveRemote ]]; then
     echo "attempting restore from ${MNB_RESTORE_LOCATION}"
     docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -e RESTORE_PATH=${MNB_RESTORE_PATH} -v "${MNB_RESTORE_LOCATION}":/opt/data mouselightdatabrowser/data-services:1.4 ./restore.sh
 else
