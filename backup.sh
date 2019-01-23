26e1691457f07aa44c64e24efdc895e1897ac45f
Patrick Edson <pedson@users.noreply.github.com>
2019-01-23 10:52:00 -0500

    Conditionals.

--- backup.sh
+++ backup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ && "options.sh" ]]; then
+if [ -a "options.sh" ]; then
     source "options.sh"
 fi
 
-if [[ test -d "${MNB_LOCAL_BACKUP_LOCATION}" ]]; then
+if [ test -d "${MNB_LOCAL_BACKUP_LOCATION}" ]; then
     echo "attempting backup to ${MNB_LOCAL_BACKUP_LOCATION}"
     docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_LOCAL_BACKUP_LOCATION}":/opt/data/ mouselightdatabrowser/data-services:1.4 ./backup.sh
 else
     echo "local backup location ${MNB_LOCAL_BACKUP_LOCATION} is not accessible - skipping"
 fi
 
-if [[ test -d "${MNB_REMOTE_BACKUP_LOCATION}" ]]; then
+if [ test -d "${MNB_REMOTE_BACKUP_LOCATION}" ]; then
     echo "attempting backup to ${MNB_REMOTE_BACKUP_LOCATION}"
     docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -e BACKUP_PATH=${MNB_REMOTE_BACKUP_PATH} -v "${MNB_REMOTE_BACKUP_LOCATION}":/opt/data mouselightdatabrowser/data-services:1.4 ./backup.sh
 else
