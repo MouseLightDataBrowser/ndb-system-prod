11e8a349bb0e99f7dcb8a36166477cb37e77be0f
Patrick Edson <pedson@users.noreply.github.com>
2024-09-30 16:11:31 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ haveLocal=(test -d "${MNB_LOCAL_BACKUP_LOCATION}")
 
 if [[ haveLocal ]]; then
     echo "attempting backup to ${MNB_LOCAL_BACKUP_LOCATION}"
-    docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_LOCAL_BACKUP_LOCATION}":/opt/data/ mouselightdatabrowser/data-services:1.7 ./backup.sh
+    docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -v "${MNB_LOCAL_BACKUP_LOCATION}":/opt/data/ mouselightdatabrowser/data-services:1.7 /bin/bash backup.sh
 else
     echo "local backup location ${MNB_LOCAL_BACKUP_LOCATION} is not accessible - skipping"
 fi
@@ -17,7 +17,7 @@ haveRemote=(test -d "${MNB_REMOTE_BACKUP_LOCATION}")
 
 if [[ haveRemote ]]; then
     echo "attempting backup to ${MNB_REMOTE_BACKUP_LOCATION}"
-    docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -e BACKUP_PATH=${MNB_REMOTE_BACKUP_PATH} -v "${MNB_REMOTE_BACKUP_LOCATION}":/opt/data mouselightdatabrowser/data-services:1.7 ./backup.sh
+    docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -e BACKUP_PATH=${MNB_REMOTE_BACKUP_PATH} -v "${MNB_REMOTE_BACKUP_LOCATION}":/opt/data mouselightdatabrowser/data-services:1.7 /bin/bash/backup.sh
 else
     echo "remote backup location ${MNB_REMOTE_BACKUP_LOCATION} is not accessible - skipping"
 fi
