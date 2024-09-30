5312f60e33ab055a619de61f2a12d7a76a1931e8
Patrick Edson <pedson@users.noreply.github.com>
2024-09-30 16:12:27 -0400

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -8,7 +8,7 @@ haveRemote=(test -d "${MNB_RESTORE_LOCATION}")
 
 if [[ haveRemote ]]; then
     echo "attempting restore from ${MNB_RESTORE_LOCATION}"
-    docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -e RESTORE_PATH=${MNB_RESTORE_PATH} -v "${MNB_RESTORE_LOCATION}":/opt/data mouselightdatabrowser/data-services:1.7 ./restore.sh
+    docker run -it --rm --network ${MNB_COMPOSE_PROJECT}_back_tier -e NODE_ENV=production -e DATABASE_PW=${DATABASE_PW} -e RESTORE_PATH=${MNB_RESTORE_PATH} -v "${MNB_RESTORE_LOCATION}":/opt/data mouselightdatabrowser/data-services:1.7 ./bin/bash restore.sh
 else
     echo "local restore location ${MNB_RESTORE_LOCATION} is not accessible - skipping"
 fi
